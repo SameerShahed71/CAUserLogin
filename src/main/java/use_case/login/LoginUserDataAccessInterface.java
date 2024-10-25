4c52843c28e7994e358b5f01ade0d6aceb2e30d3
Shahed Sameer <shahed.sameer74@gmail.com>
2024-10-25 09:39:38 -0400

    Task2.2

--- src/main/java/use_case/login/LoginUserDataAccessInterface.java
+++ src/main/java/use_case/login/LoginUserDataAccessInterface.java
@@ -27,4 +27,17 @@ public interface LoginUserDataAccessInterface {
      */
     User get(String username);
 
+    /**
+     * Sets the current user to the specified username.
+     *
+     * @param name the name of the user to set as the current user
+     */
+    void setCurrentUser(String name);
+
+    /**
+     * Gets the name of the currently logged-in user.
+     *
+     * @return the name of the current user, or null if no user is logged in
+     */
+    String getCurrentUser();
 }
